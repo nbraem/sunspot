8684f500a76f8ff4c84306a8c92437e7b556b0c8
Nicolas Braem <nicolas.braem@gmail.com>
2010-03-23 15:11:09 -0700

    Getting rid of deprecation warning on Rails3

--- sunspot_rails/lib/sunspot/rails/solr_logging.rb
+++ sunspot_rails/lib/sunspot/rails/solr_logging.rb
@@ -38,7 +38,13 @@ module Sunspot
       private
 
       def format_log_entry(message, dump = nil)
-        if ActiveRecord::Base.colorize_logging
+	begin
+	  bColorize = Rails::Subscriber.colorize_logging
+	rescue NameError # Rails 2
+	  bColorize = ActiveRecord::Base.colorize_logging
+	end
+
+	if bColorize
           message_color, dump_color = "4;32;1", "0;1"
           log_entry = "  \e[#{message_color}m#{message}\e[0m   "
           log_entry << "\e[#{dump_color}m%#{String === dump ? 's' : 'p'}\e[0m" % dump if dump
